636ce3fbcf4ef64459f19240c83faf39ddb36432
TheCat-codes <diexdev0309@gmail.com>
2025-08-04 16:20:30 -0600

    0.0.2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { router } from './Routes/routes.js'
 const app = express()
 
 app.use(cors({
-  origin: true,
+  origin: "http://localhost:5173",
   credentials: true
 }))
 app.use(express.json())
